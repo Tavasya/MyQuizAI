8c2ea27642da12c80b87a3ab6db7138b3d54442d
Tavasya Ganpati <tavasyag@gmail.com>
2024-08-05 19:30:27 -0700

    fixed issued// quiz works//need to fix ui

--- src/app/api/quizz/generate/route.ts
+++ src/app/api/quizz/generate/route.ts
@@ -8,59 +8,96 @@ import { JsonOutputFunctionsParser } from "langchain/output_parsers";
 
 import saveQuizz from "./saveToDb";
 
+export async function POST(req: NextRequest) {
+  const body = await req.formData();
+  const document = body.get("pdf");
 
-export async function POST(req: NextRequest){
-    const body = await req.formData();
-    const document = body.get("pdf");
+  try {
+    const pdfLoader = new PDFLoader(document as Blob, {
+      parsedItemSeparator: " ",
+    });
+    const docs = await pdfLoader.load();
 
+    const selectedDocuments = docs.filter(
+      (doc) => doc.pageContent !== undefined
+    );
+    const texts = selectedDocuments.map((doc) => doc.pageContent);
 
-    try{
-        const pdfLoader = new PDFLoader(document as Blob, {
-            parsedItemSeparator: " "
-        });
-        const docs = await pdfLoader.load();
-    
-        const selectedDocuments = docs.filter((doc) => doc.pageContent !== undefined);
-        const texts = selectedDocuments.map((doc) => doc.pageContent);
-    
-        const prompt = 
-        "given the text which is a summary of the document, generate a quiz based on the text. Return json only that contains a quizz object with fields: name, description and questions. The questionsis an array of objects with fields: questionText, answers. The answers is an array of objects with fields: answerText, is Correct."
-        
-if(!process.env.OPENAI_API_KEY){
-    return NextResponse.json(
-        {error: "OpenAI API key not provided"}, 
-        {status: 500});
-}
-        const model = new ChatOpenAI({
-            apiKey: process.env.OPENAI_API_KEY,
-            modelName: "gpt-4-1106-preview"
-        });
+    const prompt =
+      "given the text which is a summary of the document, generate a quiz based on the text. Return json only that contains a quizz object with fields: name, description and questions. The questions is an array of objects with fields: questionText, answers. The answers is an array of objects with fields: answerText, isCorrect.";
 
-        const message = new HumanMessage({
-            content: [
-                {
-                    type: "text",
-                    text: prompt + "\n" + texts.join("\n")
-                }
-            ]
-        })
-
-        const result = await model.invoke([message]);
-        console.log(result);
-
-        const { quizzId } = await saveQuizz(result.quizz);
-
-        return NextResponse.json(
-            {quizzId}, 
-            {status: 200});
-    }
-    catch(e:any) {
-        return NextResponse.json(
-            {error: "e.message"}, 
-            {status: 500});
- 
+    if (!process.env.OPENAI_API_KEY) {
+      return NextResponse.json(
+        { error: "OpenAI API key not provided" },
+        { status: 500 }
+      );
     }
 
+    const model = new ChatOpenAI({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+      modelName: "gpt-4-1106-preview",
+    });
 
+    const parser = new JsonOutputFunctionsParser();
+    const extractionFunctionSchema = {
+      name: "extractor",
+      description: "Extracts fields from the output",
+      parameters: {
+        type: "object",
+        properties: {
+          quizz: {
+            type: "object",
+            properties: {
+              name: { type: "string" },
+              description: { type: "string" },
+              questions: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    questionText: { type: "string" },
+                    answers: {
+                      type: "array",
+                      items: {
+                        type: "object",
+                        properties: {
+                          answerText: { type: "string" },
+                          isCorrect: { type: "boolean" },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const runnable = model
+      .bind({
+        functions: [extractionFunctionSchema],
+        function_call: { name: "extractor" },
+      })
+      .pipe(parser);
+
+    const message = new HumanMessage({
+      content: [
+        {
+          type: "text",
+          text: prompt + "\n" + texts.join("\n"),
+        },
+      ],
+    });
+
+    const result: any = await runnable.invoke([message]);
+    console.log(result);
+
+    const { quizzId } = await saveQuizz(result.quizz);
+
+    return NextResponse.json({ quizzId }, { status: 200 });
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
 }
-
